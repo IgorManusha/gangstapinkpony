2970ba144aa49ba7578e9eda8464dee1786742cd
Igor Manusha <igormanushaviktor@gmail.com>
2026-04-09 23:12:53 +0300

    add project pony

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import audioFile from "./media/trek.wav";
+import audioFile2 from "./media/secondTreck.mp3";
+import audioFile3 from "./media/Game.mp3";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2 className="mainTitleGansta">Meet Gangsta</h2>
+        <div className="decoreLine"></div>
+        <h2 className="mainTitle">Pinck Pony</h2>
+        <div className="audioContainer">
+          <audio controls>
+            <source src={audioFile} type="audio/wav" />
+          </audio>
+          <audio controls>
+            <source src={audioFile2} type="audio/wav" />
+          </audio>
+          <audio controls>
+            <source src={audioFile3} type="audio/wav" />
+          </audio>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
